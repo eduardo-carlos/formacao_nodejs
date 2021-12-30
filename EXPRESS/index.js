4375bd6222f8816e1959875ba630582087c83edd
carlos.freitas <carlos.freitas@corenmg.gov.br>
2021-12-30 12:41:36 -0300

    Alteração de Código:  Parrâmetros, query params

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -3,14 +3,33 @@ const app = express();
 
 app.get("/", function(requisicao, resposta){
    resposta.send("Bem-vindo ao guia do programador!");
+   //resposta.send("Outra resposta");
 });
 
-app.get("/blog", function(requisicao, resposta){
-    resposta.send("Bem-vindo ao meu blog!");
+app.get("/blog/:artigo?", function(requisicao, resposta){
+    
+    var artigo = requisicao.params.artigo;
+    if(artigo){
+        resposta.send("artigo: " + artigo)
+    }else{
+        resposta.send("Bem-vindo ao meu blog!");
+    }
 });
 
 app.get("/canal/youtube", function(requisicao, resposta){
-    resposta.send("Bem-vindo ao meu canal!");
+    var canal = requisicao.query["canal"];
+    if(canal){
+        resposta.send(canal);
+    }else{
+        resposta.send("Nenhum cana fornecido!");
+    }
+});
+
+app.get("/ola/:nome/:empresa", function(req, res){
+    
+    nome = req.params.nome;
+    empresa = req.params.empresa;
+    res.send("Bem vindo " + nome + " do " + empresa) ;
 });
 
 app.listen(3000, function(error){
